9a94302ea1fd715c23f2f636ba8b377bdd66ec49
yashBhaiiii <pcyash710@gmail.com>
2025-04-26 13:16:35 +0530

    initial commit

---
+++ Practical 1/a.py
@@ -0,0 +1,12 @@
+# a. Usage of methods such as floor(), ceil(), sqrt(), isqrt(), gcd() etc. 
+
+import math
+
+num1 = 3.453
+num2 = 64
+
+print("\nP.1. a) Math Library methods: ")
+print(f"\n-> floor of {num1} is: {math.floor(num1)}")
+print(f"-> ceil of {num1} is: {math.ceil(num1)}")
+print(f"-> sqrt of {num2} is: {math.sqrt(num2)}")
+print(f"-> isqrt of {num2} is: {math.isqrt(num2)}")
